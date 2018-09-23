d2e875747b7db2483288e55b862c33e6d318cd84
Adrian Drabik <adriandrabik@gmail.com>
2018-09-23 23:16:03 +0200

    add exclusion of id field in crud

--- src/main/java/pl/khuzzuk/wfrp/helper/ui/crud/Crud.java
+++ src/main/java/pl/khuzzuk/wfrp/helper/ui/crud/Crud.java
@@ -7,6 +7,11 @@ import pl.khuzzuk.messaging.Bus;
 import pl.khuzzuk.wfrp.helper.event.Event;
 import pl.khuzzuk.wfrp.helper.ui.WebComponent;
 
+import javax.persistence.Id;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class Crud<T> extends WebComponent {
     private final Bus<Event> bus;
@@ -16,7 +21,18 @@ public class Crud<T> extends WebComponent {
     public static <T> Crud<T> forBean(Class<T> beanType, Bus<Event> bus) {
         Crud<T> crud = new Crud<>(bus);
         crud.table = new Grid<>(beanType);
+        getExcludedColumns(beanType).forEach(crud.table::removeColumnByKey);
         crud.add(crud.table);
         return crud;
+    }
+
+    private static List<String> getExcludedColumns(Class<?> beanType) {
+        List<String> excludedFields = new ArrayList<>();
+        for (Field field : beanType.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Id.class)) {
+                excludedFields.add(field.getName());
+            }
+        }
+        return excludedFields;
     }
 }
