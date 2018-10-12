80da06b412ac5b5795756d467f87c9404dee8352
Adrian Drabik <adrian.drabik@silvermedia.pl>
2018-10-12 15:05:06 +0200

    add skills to profession class

--- src/main/java/pl/khuzzuk/wfrp/helper/model/professions/ProfessionClass.java
+++ src/main/java/pl/khuzzuk/wfrp/helper/model/professions/ProfessionClass.java
@@ -2,14 +2,11 @@ package pl.khuzzuk.wfrp.helper.model.professions;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import pl.khuzzuk.wfrp.helper.edit.EditorType;
 import pl.khuzzuk.wfrp.helper.edit.FormElement;
+import pl.khuzzuk.wfrp.helper.model.skill.Skill;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.Set;
 
 @Data
@@ -24,6 +21,12 @@ public class ProfessionClass {
     private String description;
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER, mappedBy = "professionClass")
     private Set<Profession> professions;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "class_skills",
+            joinColumns = @JoinColumn(name = "class_id"),
+            inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    @FormElement(editor = EditorType.CHOOSE)
+    private Set<Skill> skills;
 
     @Override
     public String toString() {
