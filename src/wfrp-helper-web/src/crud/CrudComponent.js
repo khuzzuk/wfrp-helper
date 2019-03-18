f6298f30f8792bc7395ffc40955dbbb870a4bae8
Adrian Drabik <adriandrabik@gmail.com>
2019-03-18 19:45:37 +0100

    work on CrudComponent

--- src/wfrp-helper-web/src/crud/CrudComponent.js
+++ src/wfrp-helper-web/src/crud/CrudComponent.js
@@ -1,29 +1,54 @@
 import React, {Component} from 'react';
-import {Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
+import {Button, Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
 
 class CrudComponent extends Component {
+    state = {
+        selectedId: null
+    };
+
+    onRowSelect = id => {
+        this.setState({selectedId: id});
+        let row = this.props.rows.find(row => row.id === id);
+        console.log(row);
+    };
+
     render() {
         const {columns, rows} = this.props;
+        let crudButtons = <div/>;
+        if (columns.length > 0) {
+            crudButtons =
+                <div>
+                    <Button>Add</Button>
+                    <Button disabled={this.state.selectedId === null}>Edit</Button>
+                    <Button disabled={this.state.selectedId === null}>Remove</Button>
+                </div>
+        }
+
         return <div>
+            {crudButtons}
             <Table>
                 <TableHead>
-                    {columns.map(column => (
-                        <TableCell align={'right'}>{column.header}</TableCell>
-                    ))}
+                    <TableRow>
+                        {columns.map(column => (
+                            <TableCell key={column.header} align={'right'}>{column.header}</TableCell>
+                        ))}
+                    </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map(row => (
-                        <TableRow>
+                    {rows.map((row, index) => (
+                        <TableRow key={row.id} hover
+                                  onClick={event => this.onRowSelect(row.id)}
+                                  selected={this.state.selectedId === row.id}>
                             {
                                 columns.map(col => {
-                                    return <TableCell>{row[col.field]}</TableCell>
+                                    return <TableCell key={row.id + '_' + col.field}>{row[col.field]}</TableCell>
                                 })
                             }
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-        </div>
+        </div>;
     }
 }
 
