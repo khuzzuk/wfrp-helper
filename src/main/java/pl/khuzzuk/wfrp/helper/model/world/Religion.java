18a911995beb1813df32cebc82247d57573ea570
Adrian Drabik <adriandrabik@gmail.com>
2020-07-25 19:31:06 +0200

    migrate to maven for gcp

--- src/main/java/pl/khuzzuk/wfrp/helper/model/world/Religion.java
+++ src/main/java/pl/khuzzuk/wfrp/helper/model/world/Religion.java
@@ -1,13 +1,20 @@
 package pl.khuzzuk.wfrp.helper.model.world;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.NaturalId;
 import pl.javahello.RemoteEntity;
-
-import javax.persistence.*;
-import java.util.List;
 import pl.javahello.RemoteEntity.SecuredService;
 
 @Getter
@@ -18,17 +25,22 @@ import pl.javahello.RemoteEntity.SecuredService;
 @RemoteEntity(transactional = true)
 @SecuredService(allowRead = true)
 public class Religion {
-    @SequenceGenerator(name = "religion_id_seq_gen", schema = "world", sequenceName = "religion_id_seq")
-    @GeneratedValue(generator = "religion_id_seq_gen", strategy = GenerationType.SEQUENCE)
-    @Id
-    private Long id;
-    @NaturalId
-    private String name;
-    private String description;
-    @ManyToMany
-    @JoinTable(schema = "world", name = "religion_nation",
-            joinColumns = @JoinColumn(name = "religion_id"),
-            inverseJoinColumns = @JoinColumn(name = "nation_id"))
-    private List<Nation> nations;
+
+  @SequenceGenerator(name = "religion_id_seq_gen",
+                     schema = "world",
+                     sequenceName = "religion_id_seq",
+                     allocationSize = 1)
+  @GeneratedValue(generator = "religion_id_seq_gen", strategy = GenerationType.SEQUENCE)
+  @Id
+  private Long id;
+  @NaturalId
+  private String name;
+  private String description;
+  @ManyToMany
+  @JoinTable(schema = "world",
+             name = "religion_nation",
+             joinColumns = @JoinColumn(name = "religion_id"),
+             inverseJoinColumns = @JoinColumn(name = "nation_id"))
+  private List<Nation> nations;
 
 }
